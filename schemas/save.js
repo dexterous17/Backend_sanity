c3a4faaeceabd82f44659e206a8f55a5e71028aa
IamDexterous <harshilprajapatiofficial@gmail.com>
2022-04-29 00:56:43 -0300

    Well have to create an different Git repo for backend for the application because of the problems that Github is giving me

---
+++ schemas/save.js
@@ -0,0 +1,24 @@
+export default {
+    name: "save",
+    title: "Save",
+    type: "document",
+    fields: [
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'document',
+            fields: [
+                {
+                    name: 'postedBy',
+                    title: 'PostedBy',
+                    type: 'postedBy'
+                },
+                {
+                    name: 'userId',
+                    title: 'Comment',
+                    type: 'string'
+                }
+            ]
+        }
+    ]
+}
---
+++ schemas/schema.js
@@ -0,0 +1,17 @@
+import createSchema from 'part:@sanity/base/schema-creator'
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+import user from './user'
+import pin from './pin'
+import comment from './comment'
+import postedBy from './postedBy'
+import save from './save'
+
+export default createSchema({
+  // We name our schema
+  name: 'default',
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    user, pin, comment, postedBy, save
+  ]),
+})
